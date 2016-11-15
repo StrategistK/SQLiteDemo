53124346eedc616d1ecd89eaf54e1079bde7d182
strategist k <815034557@qq.com>
2016-11-15 11:50:07 +0800

    SQLite
    SQLite CRUD 练习/query()学习
    C-create
    R-retrieve
    U-update
    D-delete

--- app/src/main/java/com/study/android/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/study/android/sqlitedemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.study.android.sqlitedemo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private MySQLiteOpenHelper mMySQLiteOpenHelper;
+    private Button mCreateDatabase;
+    private Button mAddData;
+    private Button mUpdateData;
+    private Button mDeleteData;
+    private Button mQueryData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,82 @@ public class MainActivity extends AppCompatActivity {
 
         mMySQLiteOpenHelper = new MySQLiteOpenHelper(this,"BookStore.db",null,2);
 
-        Button createDatabase = (Button)findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
+        mCreateDatabase = (Button)findViewById(R.id.create_database);
+        mCreateDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mMySQLiteOpenHelper.getWritableDatabase();
+            }
+        });
+
+        mAddData = (Button)findViewById(R.id.add_data);
+        mAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = mMySQLiteOpenHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+//                start first values
+                values.put("name", "The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages", 454);
+                values.put("price",16.95);
+//                insert first values
+                db.insert("Book",null,values);
+//                clear first values
+                values.clear();
+//                start second values
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",19.95);
+//                insert second values
+                db.insert("Book",null,values);
+            }
+        });
+
+        mUpdateData = (Button)findViewById(R.id.update_data);
+        mUpdateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = mMySQLiteOpenHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name = ?",new String[]{"The Da Vinci Code"});
+            }
+        });
+
+        mDeleteData = (Button)findViewById(R.id.delete_data);
+        mDeleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = mMySQLiteOpenHelper.getWritableDatabase();
+                db.delete("Book","pages > ?",new String[]{"500"});
+            }
+        });
+
+        mQueryData = (Button)findViewById(R.id.query_data);
+        mQueryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = mMySQLiteOpenHelper.getWritableDatabase();
+//                查询Book中所有的数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()) {
+                    do {
+//                        遍历Cursor对象，取出数据
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+//                        打印数据
+                        Log.d("MainActivity","book name is " + name);
+                        Log.d("MainActivity","book author is " + author);
+                        Log.d("MainActivity","book pages is " + pages);
+                        Log.d("MainActivity","book price is " + price);
+
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
